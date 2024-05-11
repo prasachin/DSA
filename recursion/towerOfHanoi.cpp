251d9bdc379c69a2d93691c6f14ea8dbee143208
sachin <sachinp@iitbhilai.ac.in>
2024-05-11 17:35:35 +0530

    codes

---
+++ recursion/towerOfHanoi.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define vvll vector<vector<long long>>
+#define vpi vector<pair<int, int>>
+#define vpll vector<pair<long long, long long>>
+#define mii map<int, int>
+#define mis map<int, string>
+#define msi map<string, int>
+#define si set<int>
+#define sll set<long long>
+#define sc set<char>
+/* FUNCTIONS */
+#define f(i, s, e) for (long long int i = s; i < e; i++)
+#define rf(i, e, s) for (long long int i = e - 1; i >= s; i--)
+/* PRINTS */
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p)
+{
+    return os << '(' << p.first << "," << p.second << ')';
+}
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os;
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = " ";
+    return os;
+}
+/* UTILS */
+#define M 1000000007
+string to_upper(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'a' && a[i] <= 'z')
+            a[i] -= 'a' - 'A';
+    return a;
+}
+string to_lower(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'A' && a[i] <= 'Z')
+            a[i] += 'a' - 'A';
+    return a;
+}
+bool chk_pali(string s)
+{
+    for (int i = 0; i < (int)s.length() / 2; i++)
+        if (s[i] != s[s.length() - 1 - i])
+            return 0;
+    return 1;
+}
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+#define all(a) (a).begin(), (a).end()
+
+// Q.we are given n number of disc and we are called to move all these to the tower C from the tower A there is also a tower B inbetween ;
+// rules:
+// 1: only one disc can be moved at once
+// 2: only the top disc can be carried out at once ;
+// 3:final arrangement should be in the same order
+
+void toh(int n, char a, char b, char c)
+{
+    if (n == 1)
+    {
+        cout << "Move 1 from " << a << " to " << c << endl;
+        return;
+    }
+    toh(n - 1, a, c, b);
+    cout << "Move " << n << " from " << a << " to " << c << endl;
+    toh(n - 1, b, a, c);
+}
+
+void solve()
+{
+    int n;
+    char a, b, c;
+    cin >> n;
+    cin >> a >> b >> c;
+    toh(n, a, b, c);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
