028cbd05f17c518eeedea98b1ca2e5cdfb4480ae
sachin <sachinp@iitbhilai.ac.in>
2024-06-15 14:46:12 +0530

    codes

---
+++ graph/tarjansAlgo.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define vvll vector<vector<long long>>
+#define vpi vector<pair<int, int>>
+#define vpll vector<pair<long long, long long>>
+#define mii map<int, int>
+#define mis map<int, string>
+#define msi map<string, int>
+#define si set<int>
+#define sll set<long long>
+#define sc set<char>
+/* FUNCTIONS */
+#define f(i, s, e) for (long long int i = s; i < e; i++)
+#define rf(i, e, s) for (long long int i = e - 1; i >= s; i--)
+/* PRINTS */
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p)
+{
+    return os << '(' << p.first << "," << p.second << ')';
+}
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os;
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = " ";
+    return os;
+}
+/* UTILS */
+#define M 1000000007
+string to_upper(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'a' && a[i] <= 'z')
+            a[i] -= 'a' - 'A';
+    return a;
+}
+string to_lower(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'A' && a[i] <= 'Z')
+            a[i] += 'a' - 'A';
+    return a;
+}
+bool chk_pali(string s)
+{
+    for (int i = 0; i < (int)s.length() / 2; i++)
+        if (s[i] != s[s.length() - 1 - i])
+            return 0;
+    return 1;
+}
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+#define all(a) (a).begin(), (a).end()
+
+void addedge(vector<int> adj[], int u, int v)
+{
+    adj[v].push_back(u);
+    adj[u].push_back(v);
+}
+
+void APUtil(vector<int> adj[], int u, bool visited[],
+            int disc[], int low[], int &time, int parent,
+            bool isAP[])
+{
+    // Count of children in DFS Tree
+    int children = 0;
+
+    // Mark the current node as visited
+    visited[u] = true;
+
+    // Initialize discovery time and low value
+    disc[u] = low[u] = ++time;
+
+    // Go through all vertices adjacent to this
+    for (auto v : adj[u])
+    {
+        // If v is not visited yet, then make it a child of u
+        // in DFS tree and recur for it
+        if (!visited[v])
+        {
+            children++;
+            APUtil(adj, v, visited, disc, low, time, u, isAP);
+
+            // Check if the subtree rooted with v has
+            // a connection to one of the ancestors of u
+            low[u] = min(low[u], low[v]);
+
+            // If u is not root and low value of one of
+            // its child is more than discovery value of u.
+            if (parent != -1 && low[v] >= disc[u])
+                isAP[u] = true;
+        }
+
+        // Update low value of u for parent function calls.
+        else if (v != parent)
+            low[u] = min(low[u], disc[v]);
+    }
+
+    // If u is root of DFS tree and has two or more children.
+    if (parent == -1 && children > 1)
+        isAP[u] = true;
+}
+
+void AP(vector<int> adj[], int V)
+{
+    int disc[V] = {0};
+    int low[V];
+    bool visited[V] = {false};
+    bool isAP[V] = {false};
+    int time = 0, par = -1;
+
+    // Adding this loop so that the
+    // code works even if we are given
+    // disconnected graph
+    for (int u = 0; u < V; u++)
+        if (!visited[u])
+            APUtil(adj, u, visited, disc, low,
+                   time, par, isAP);
+
+    // Printing the APs
+    for (int u = 0; u < V; u++)
+        if (isAP[u] == true)
+            cout << u << " ";
+}
+void solve()
+{
+    cout << "Enter the number of the vertices " << endl;
+    int v;
+    cin >> v;
+    vector<int> adj[v];
+    cout << "Enter the edge number :" << endl;
+    int b;
+    cin >> b;
+    for (int i = 0; i < b; i++)
+    {
+        cout << "Enter u and v" << endl;
+        int u, w;
+        cin >> u >> w;
+        addedge(adj, u, w);
+    }
+    cout << "The articulation point of the given graph will be :" << endl;
+    AP(adj, v);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
